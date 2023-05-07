5ca87510788c19b1f8e7da41719cb85e895761ec
harikishantk <harikishantk@gmail.com>
2023-05-07 23:16:30 +0530

    Add: Send Frames and save the image (#3 and #4)

--- webcam/consumers.py
+++ webcam/consumers.py
@@ -1,15 +1,26 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
+from io import BytesIO
+from PIL import Image
+import base64
 
 class StreamConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        print("connected")
         await self.accept()
         
     async def disconnect(self, close_code):
-        print("disconnected")
         pass
         
     async def receive(self, text_data):
-        # Do something with the received message
-        print("received")
-        pass
+        if text_data == 'start_stream':
+            # Do something when the webcam stream starts
+            pass
+        else:
+            # Convert the received base64 encoded JPEG image to a PIL image
+            data = BytesIO(base64.b64decode(text_data.split(',')[1]))
+            image = Image.open(data)
+
+            # Save the image to the media directory
+            image.save('media/stream.jpg')
+            
+            # Do something with the received image
+            pass
